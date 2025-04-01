9f7ad40290e0dcf9484aceb619fb81df2dba61ce
Divyanshu81 <divyanshu52singhal@gmail.com>
2025-04-01 17:48:22 +0530

    Requirements.txt

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import subprocess
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify,send_file
 from collections import defaultdict
 from datetime import datetime, timedelta
